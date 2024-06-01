946b56adac47d58de3a73e9da5d90f82291a0c57
Libin S <117743766+Libin-4821@users.noreply.github.com>
2024-06-01 12:36:25 +0530

    Add files via upload

---
+++ disk_scheduling_programs/Clook.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>  // For abs() function
+
+int main() {
+    int t[20], n, i, j, h, tot = 0, temp, pos;
+    int atr[20], p = 0;
+
+    printf("Enter the number of tracks to be traversed: ");
+    scanf("%d", &n);
+
+    printf("Enter the position of head: ");
+    scanf("%d", &h);
+
+    t[0] = h;
+
+    printf("Enter the tracks to be traversed: ");
+    for (i = 1; i <= n; i++) {
+        scanf("%d", &t[i]);
+    }
+
+    // Sort the tracks
+    for (i = 0; i <= n; i++) {
+        for (j = i + 1; j <= n; j++) {
+            if (t[i] > t[j]) {
+                temp = t[i];
+                t[i] = t[j];
+                t[j] = temp;
+            }
+        }
+    }
+
+    // Find the position of the head in the sorted array
+    for (i = 0; i <= n; i++) {
+        if (t[i] == h) {
+            pos = i;
+            break;
+        }
+    }
+
+    // Traverse towards the end of the list
+    for (i = pos; i <= n; i++) {
+        atr[p++] = t[i];
+    }
+    // Directly move to the beginning of the list and continue traversal
+    for (i = 0; i < pos; i++) {
+        atr[p++] = t[i];
+    }
+
+    printf("Tracks traversed:\n");
+    for (i = 0; i < p; i++) {
+        printf("%d ", atr[i]);
+    }
+    printf("\n");
+
+    printf("Difference between tracks:\n");
+    for (i = 0; i < p - 1; i++) {
+        tot += abs(atr[i + 1] - atr[i]);
+        printf("%d %d\n", atr[i], abs(atr[i + 1] - atr[i]));
+    }
+
+    printf("Total header movements: %d\n", tot);
+    printf("Average seek time: %f\n", (float)tot / n);
+
+    return 0;
+}
